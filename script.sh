05e3a847248e36b2128eb5bd3bd9e391f1472aeb
cpm1986 <cpm1986@gmail.com>
2020-10-28 15:15:27 +0100

    primer commit v3

--- script.sh
+++ script.sh
@@ -1,1 +1,10 @@
-echo hola;
+#!/bin/bash
+echo hola
+read -n 7 -p "Dime tu nombre: " nombre
+if [ "$nombre" != "" ]
+then
+    echo "Hola $nombre"
+else
+    echo "Pues si no me quires decir tu nombre peor para ti"
+fi
+
